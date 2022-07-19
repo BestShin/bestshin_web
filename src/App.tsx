5850ae40ce80d3a47b09d6bdd6926d0045dd33c3
최민우 <woasidh1@naver.com>
2022-07-19 23:38:40 +0900

    eslint & husky 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const lintTestFnc = () => {
+    let a = 1;
+    let b = 2;
+    a += 1;
+    b += 2;
+    if (a == b) console.log('a equals b');
+  }
+
   return (
     <div className='App'>
       <span>Hello MUG!</span>
